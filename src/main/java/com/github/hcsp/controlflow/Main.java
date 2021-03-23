fdab4864cdf227d23495e7a757acabe4af40298b
bujidefeng <1587770721@qq.com>
2021-03-23 13:49:19 +0800

    编写阶乘函数

--- src/main/java/com/github/hcsp/controlflow/Main.java
+++ src/main/java/com/github/hcsp/controlflow/Main.java
@@ -14,5 +14,16 @@ public class Main {
      * @param n 输入数字
      * @return 该数字的阶乘
      */
-    public static int factorial(int n) {}
+    public static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }else {
+            int a = n ;
+            while (n > 1){
+               a = a * (n - 1);
+                n--;
+            }
+            return a;
+        }
+    }
 }
